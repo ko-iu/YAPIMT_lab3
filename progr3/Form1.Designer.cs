4213386a43d851ee72c7eb88e50e8abcf99eca3a
Iuliana <krnlviu@gmail.com>
2025-04-16 18:52:20 +0900

    Add project files.

---
+++ progr3/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace progr3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Ввод текста";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(40, 37);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(578, 234);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(696, 112);
+            button1.Name = "button1";
+            button1.Size = new Size(200, 34);
+            button1.TabIndex = 2;
+            button1.Text = "Анализировать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(42, 276);
+            label2.Name = "label2";
+            label2.Size = new Size(157, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Служебные слова";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(308, 276);
+            label3.Name = "label3";
+            label3.Size = new Size(207, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Операции и отношения";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(599, 276);
+            label4.Name = "label4";
+            label4.Size = new Size(201, 25);
+            label4.TabIndex = 5;
+            label4.Text = "Специальные символы";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(42, 545);
+            label5.Name = "label5";
+            label5.Size = new Size(193, 25);
+            label5.TabIndex = 6;
+            label5.Text = "Строковые константы";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(308, 545);
+            label6.Name = "label6";
+            label6.Size = new Size(183, 25);
+            label6.TabIndex = 7;
+            label6.Text = "Числовые константы";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(599, 545);
+            label7.Name = "label7";
+            label7.Size = new Size(153, 25);
+            label7.TabIndex = 8;
+            label7.Text = "Идентификаторы";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(42, 314);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(157, 233);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(308, 314);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(207, 233);
+            textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(599, 314);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(201, 233);
+            textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(42, 573);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(193, 209);
+            textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(308, 573);
+            textBox6.Multiline = true;
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(207, 209);
+            textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(599, 573);
+            textBox7.Multiline = true;
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(198, 209);
+            textBox7.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(996, 794);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            WindowState = FormWindowState.Maximized;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+    }
+}
---
+++ progr3/Form1.cs
@@ -0,0 +1,293 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace progr3
+{
+    public partial class Form1 : Form
+    {
+        private string inputText;
+        private int currentPosition;
+        private StringBuilder buffer;
+        private List<string> keywords = new List<string>();
+        private List<string> operators = new List<string>();
+        private List<string> specialSymbols = new List<string>();
+        private List<string> stringConstants = new List<string>();
+        private List<string> numericConstants = new List<string>();
+        private List<string> identifiers = new List<string>();
+
+      
+        private readonly HashSet<string> csharpKeywords = new HashSet<string>
+        {
+            "abstract", "as", "base", "bool", "break", "byte", "case", "catch",
+            "char", "checked", "class", "const", "continue", "decimal", "default",
+            "delegate", "do", "double", "else", "enum", "event", "explicit",
+            "extern", "false", "finally", "fixed", "float", "for", "foreach",
+            "goto", "if", "implicit", "in", "int", "interface", "internal",
+            "is", "lock", "long", "namespace", "new", "null", "object", "operator",
+            "out", "override", "params", "private", "protected", "public",
+            "readonly", "ref", "return", "sbyte", "sealed", "short", "sizeof",
+            "stackalloc", "static", "string", "struct", "switch", "this", "throw",
+            "true", "try", "typeof", "uint", "ulong", "unchecked", "unsafe",
+            "ushort", "using", "var", "virtual", "void", "volatile", "while",
+            "begin", "end", "to", "do", "integer", "writeln"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            buffer = new StringBuilder();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AnalyzeCode();
+        }
+
+        private void AnalyzeCode()
+        {
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            keywords.Clear();
+            operators.Clear();
+            specialSymbols.Clear();
+            stringConstants.Clear();
+            numericConstants.Clear();
+            identifiers.Clear();
+
+            inputText = textBox1.Text;
+            currentPosition = 0;
+
+            if (string.IsNullOrEmpty(inputText)) return;
+        
+            inputText += "\uFFFF";
+            int type = 0;
+            while (StepLexAnalyze(ref type) > 0) { }
+            DisplayResults();
+        }
+
+        private char GetInputChar()
+        {
+            if (currentPosition < inputText.Length)
+                return inputText[currentPosition++];
+            return '\0';
+        }
+
+        private void RevLit(char c)
+        {
+            if (currentPosition > 0) currentPosition--;
+        }
+
+        private void PutInBuf(char c)
+        {
+            buffer.Append(c);
+        }
+
+        private bool Number(char c)
+        {
+            return char.IsDigit(c);
+        }
+
+        private bool Letter(char c)
+        {
+            return char.IsLetter(c) || c == '_';
+        }
+
+        private int Buf_Intend_Key(ref int type)
+        {
+            string lexeme = buffer.ToString();
+            buffer.Clear();
+
+            if (csharpKeywords.Contains(lexeme))
+            {
+                if (!keywords.Contains(lexeme))
+                    keywords.Add(lexeme);
+                type = 1; // Ключевое слово
+                return keywords.IndexOf(lexeme) + 1;
+            }
+            else
+            {
+                if (!identifiers.Contains(lexeme))
+                    identifiers.Add(lexeme);
+                type = 4; // Идентификатор
+                return identifiers.IndexOf(lexeme) + 1;
+            }
+        }
+
+        private int Buf_Oper(ref int type)
+        {
+            string lexeme = buffer.ToString();
+            buffer.Clear();
+
+            if (!operators.Contains(lexeme))
+                operators.Add(lexeme);
+            type = 2; // Оператор
+            return operators.IndexOf(lexeme) + 1;
+        }
+
+        private int Buf_Spec(ref int type)
+        {
+            string lexeme = buffer.ToString();
+            buffer.Clear();
+
+            if (!specialSymbols.Contains(lexeme))
+                specialSymbols.Add(lexeme);
+            type = 3; // Специальный символ
+            return specialSymbols.IndexOf(lexeme) + 1;
+        }
+
+        private int Buf_Num(ref int type)
+        {
+            string lexeme = buffer.ToString();
+            buffer.Clear();
+
+            if (!numericConstants.Contains(lexeme))
+                numericConstants.Add(lexeme);
+            type = 5; // Числовая константа
+            return numericConstants.IndexOf(lexeme) + 1;
+        }
+
+        private int Buf_Str(ref int type)
+        {
+            string lexeme = buffer.ToString();
+            buffer.Clear();
+            if (lexeme.Length >= 2 && (lexeme[0] == '"' || lexeme[0] == '\'') &&
+                lexeme[lexeme.Length - 1] == lexeme[0])
+            {
+                lexeme = lexeme.Substring(1, lexeme.Length - 2);
+            }
+
+            if (!stringConstants.Contains(lexeme))
+                stringConstants.Add(lexeme);
+            type = 6; // Строковая константа
+            return stringConstants.IndexOf(lexeme) + 1;
+        }
+
+        private int StepLexAnalyze(ref int type)
+        {
+            char c;
+            State T = State.S;
+
+            do
+            {
+                c = GetInputChar();
+                switch (T)
+                {
+                    case State.S:
+                        if (c == ' ' || c == '\t' || c == '\r' || c == '\n') break;
+                        if (c == '{') { T = State.cm; break; }
+                        if (c == '"' || c == '\'') { PutInBuf(c); T = State.st; break; }
+                        if (c == '\uFFFF') return 0;
+
+                        PutInBuf(c);
+                        if (Letter(c)) { T = State.id; break; }
+                        if (c == '=' || c == '+' || c == '-' || c == '*' || c == '/' ||
+                            c == '%' || c == '<' || c == '>' || c == '!' || c == '&' || c == '|')
+                        {
+                            
+                            char nextChar = GetInputChar();
+                            if ((c == ':' && nextChar == '=') ||
+                                (c == '<' && nextChar == '=') ||
+                                (c == '>' && nextChar == '=') ||
+                                (c == '=' && nextChar == '=') ||
+                                (c == '!' && nextChar == '=') ||
+                                (c == '&' && nextChar == '&') ||
+                                (c == '|' && nextChar == '|'))
+                            {
+                                PutInBuf(nextChar);
+                            }
+                            else
+                            {
+                                RevLit(nextChar);
+                            }
+                            return Buf_Oper(ref type);
+                        }
+                        if (c == ':') { T = State.ss; break; }
+                        if (c == '.' || c == ';' || c == ',' || c == '(' || c == ')' ||
+                            c == '[' || c == ']' || c == '{' || c == '}')
+                            return Buf_Spec(ref type);
+                        if (Number(c)) { T = State.cf; break; }
+                        return -1;
+
+                    case State.id:
+                        if (Number(c) || Letter(c)) { PutInBuf(c); break; }
+                        else { RevLit(c); return Buf_Intend_Key(ref type); }
+
+                    case State.ss:
+                        if (c == '=')
+                        {
+                            PutInBuf(c);
+                            return Buf_Oper(ref type);
+                        }
+                        RevLit(c);
+                        return Buf_Spec(ref type);
+
+                    case State.cf:
+                        if (Number(c) || c == '.')
+                        {
+                            PutInBuf(c);
+                            break;
+                        }
+                        RevLit(c);
+                        return Buf_Num(ref type);
+
+                    case State.st:
+                        if (c == '"' || c == '\'')
+                        {
+                            PutInBuf(c);
+                            return Buf_Str(ref type);
+                        }
+                        PutInBuf(c);
+                        break;
+                }
+            } while (true);
+        }
+
+        private void DisplayResults()
+        {
+            // Служебные слова
+            for (int i = 0; i < keywords.Count; i++)
+            {
+                textBox2.AppendText($"{i + 1} {keywords[i]}{Environment.NewLine}");
+            }
+
+            // Операции и отношения
+            for (int i = 0; i < operators.Count; i++)
+            {
+                textBox3.AppendText($"{i + 1} {operators[i]}{Environment.NewLine}");
+            }
+
+            // Специальные символы
+            for (int i = 0; i < specialSymbols.Count; i++)
+            {
+                textBox4.AppendText($"{i + 1} {specialSymbols[i]}{Environment.NewLine}");
+            }
+
+            // Строковые константы
+            for (int i = 0; i < stringConstants.Count; i++)
+            {
+                textBox5.AppendText($"{i + 1} {stringConstants[i]}{Environment.NewLine}");
+            }
+
+            // Числовые константы
+            for (int i = 0; i < numericConstants.Count; i++)
+            {
+                textBox6.AppendText($"{i + 1} {numericConstants[i]}{Environment.NewLine}");
+            }
+
+            // Идентификаторы
+            for (int i = 0; i < identifiers.Count; i++)
+            {
+                textBox7.AppendText($"{i + 1} {identifiers[i]}{Environment.NewLine}");
+            }
+        }
+
+        private enum State { S, cm, id, ss, cf, st, dp, fin }
+    }
+}
